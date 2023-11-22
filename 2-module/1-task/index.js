14bd64fe73d3b32fec39adb0e3e34c8678e99667
HappyG1lmore <cereperec@gmail.com>
2023-11-22 13:57:40 +0300

    task2_obj

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,10 @@
 function sumSalary(salaries) {
-  // ваш код...
+  let sum = 0;
+  for (key in salaries) {
+    if (typeof salaries[key] == "number" && isFinite(salaries[key])) {
+      sum += salaries[key];
+    }
+  }
+  console.log(sum);
+  return sum;
 }
--- 2-module/2-task/index.js
+++ 2-module/2-task/index.js
@@ -1,3 +1,12 @@
 function isEmpty(obj) {
-  // ваш код...
+  let j = 0;
+  for (key in obj) {
+    j += 1;
+  }
+  return j == 0;
+}
+
+// первое сам придумыва. ДЛя второго варианта нашел метод в интернете, вроде тоже работает
+function isEmpty2(obj) {
+  return Object.keys(obj).length == 0;
 }
