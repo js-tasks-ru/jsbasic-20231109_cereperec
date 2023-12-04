382a77ce8a337a005ba309fe0dad1c6b54e708b9
HappyG1lmore <cereperec@gmail.com>
2023-12-04 20:05:21 +0300

    fixed task3, and task4

--- 3-module/3-task/index.js
+++ 3-module/3-task/index.js
@@ -1,11 +1,8 @@
 function camelize(str) {
-  let arr = str.split("-");
-  let newArr = [];
-  arr.forEach((element, index) => {
+  return str.split("-").map((element, index) => {
     if (index > 0) {
       element = element[0].toUpperCase() + element.slice(1);
     }
-    newArr.push(element);
-  });
-  return newArr.join('');
+    return element;
+  }).join('');
 }
--- 4-module/1-task/index.js
+++ 4-module/1-task/index.js
@@ -1,3 +1,8 @@
 function makeFriendsList(friends) {
-  // ваш код...
+  const ul = document.createElement('ul');
+  document.body.append(ul);
+  friends.forEach(friend => {
+    ul.innerHTML += `<li style="list-style-type:none">${friend.firstName} ${friend.lastName}</li>`;
+  });
+  return ul;
 }
